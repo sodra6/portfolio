ddc678fc46eea61ead0ab378ccae6d7410dab473
sodra6 <sodra6@naver.com>
2023-03-06 17:14:22 +0900

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = app => {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://port-0-node-server-study-r8xoo2mleme9svb.sel3.cloudtype.app/:3000",
+      target: "https://port-0-node-server-study-r8xoo2mleme9svb.sel3.cloudtype.app",
       changeOrigin: true,
     })
   );
